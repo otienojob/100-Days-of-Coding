fdc9def7da15ea0228a318ae97ee9f7d163a901d
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2024-02-20 10:45:11 +0300

    Cake mix recipe added

---
+++ Day 1/pythonProject1/Day 1/L2- Simple Print Statement.py
@@ -0,0 +1,6 @@
+#simple print output
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Leave it to rise for 2 hours")
+print("4. Bake at 200 degrees C for 30 minutes")
