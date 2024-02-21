ddddfbebb63bb584e0cb3133338e732d43cb6278
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2024-02-21 16:19:46 +0300

    Create & initialize file

--- Day 2/D2 - Tip Calculator.py
+++ Day 2/D2 - Tip Calculator.py
@@ -1,9 +1,18 @@
 #calculate amount to pay
 #calculate amount to pay + tip
 
-bill_amount=3
+#capture table details
+table_host=input(print("Welcome to the Manour Eatery! What's your name? "))
+print(f"Welcom Mr./Ms. {table_host} and settle at table 10 \n")
+
+#bill_amount=3
 #tip_percent:float
 #tip_amount:float
-total_amount:float
+#total_amount:float
 
-print(type(bill_amount))
+bill_response=input(print('Are you ready to settle your bill {table_host} \n:'))
+
+if bill_response.upper()== "Y":
+    print("Your Bill is: ")
+
+
