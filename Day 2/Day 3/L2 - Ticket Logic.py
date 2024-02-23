dbe43328154cd87a00832ee0ab659218b77f4482
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2024-02-23 11:29:40 +0300

    + Added Photo Logic
    + Added price variable

--- Day 2/Day 3/L2 - Ticket Logic.py
+++ Day 2/Day 3/L2 - Ticket Logic.py
@@ -1,14 +1,19 @@
 #ticketing logic
+ticket_price=0
 u_name=input(print('Hello! Welcome to the "Ticketing Booth", What is your name?'))
 height=int(input(print(f"\t{u_name}, please enter your height (cm) as a whole number!")))
 if height > 120:
     user_age = int(input(print("\tKindly can you provide us with your age? ")))
     if user_age <= 12:
-        print(f"\tTicket pricing for your age category {u_name} is $5.00")
+        ticket_price=5
+        print(f'\tTicket Category: "Child" pricing for your age category {u_name} is ', ticket_price)
     elif user_age>12 and user_age<18:
-        print(f"\t\tTicket pricing for your age category {u_name} is $7.00 ", )
+        ticket_price=7
+        print(f'\t\tTicket Category: "Youth" pricing for your age category {u_name} is ', ticket_price )
     else:
-        print(f'\t\tThe ticket cost for your age category{u_name} is $12.00 [full adult price')
+        ticket_price=12
+        print(f'\t\tTicket Category: "Adult" cost for your age category{u_name} is', ticket_price)
+    pic_request=str(input(print("Would you like a photo of your ride experience? ")))
 else:
     print("Sorry, you cannot go onto the ride")
 
