bd95fd99e11b5c4f3fc9fb8a33a3b29fdbd2b5c0
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2024-02-23 16:26:51 +0300

    Finally fixed the conditional issues

--- Day 2/Day 3/L5 - Pizza.py
+++ Day 2/Day 3/L5 - Pizza.py
@@ -1,38 +1,42 @@
-#Pizza Order Module
-visual=2
-p_price=0
-p_store=["S","M","L"]
-print(visual**5 * '-')
+# Pizza Order Module
+visual = 2
+p_price = 0
+p_store = ["S", "M", "L"]
+
+print(visual ** 10 * '-')
 print("Welcome to the Python Pizza Deliveries\n\n")
-print(visual**5 * '-')
+print(visual ** 10 * '-')
 print("Base price set to 0")
-req_inp=input(print("\nDo we proceed to take your order? "))
-if req_inp.upper()=="Y":
-    p_size=input(print(f'What Pizza Size Would You Like to Order - {p_store}'))
-    if p_size.upper()=="S":
+req_inp = input(print("Do we proceed to take your order? "))
+if req_inp.upper() == "Y":
+    p_size = input(print(f'What Pizza Size Would You Like to Order - {p_store}'))
+    if p_size.upper() == "S":
         p_price += 15
-        print("Current Bill: $",p_price)
-    elif p_size.upper()=="M":
+        print("Current Bill: $", p_price)
+    elif p_size.upper() == "M":
         p_price += 20
-        print("Current Bill: $",p_price)
+        print("Current Bill: $", p_price)
     else:
         p_price += 25
-        print("Current Bill: $",p_price)
-    add_pepperoni=input(print('Would you like some extra pepperoni? "Y" for "Yes" and "N" for "No"' ))
+        print("Current Bill: $", p_price)
+    print(visual ** 10 * '-')
+    add_pepperoni = input(print('Would you like some extra pepperoni? "Y" for "Yes" and "N" for "No"'))
     if add_pepperoni.upper() == "Y":
-        pep_size=input(print(f'Add for what size: {p_store}? '))
+        pep_size = input(print(f'Add for what size: {p_store}? '))
         if pep_size.upper() == "S":
-            p_price+=2
+            p_price += 2
             print(f'Unit price with pepperoni added is {p_price}')
         else:
-            p_price+=3
+            p_price += 3
             print(f'Unit price with pepperoni added is {p_price}')
-    else:
-        print("Would you like some extra cheese?")
+
+    print(visual ** 10 * '-')
     add_cheese = input(print('Would you like some extra cheese? "Y" for "Yes" and "N" for "No"'))
     if add_cheese.upper() == 'Y':
         p_price += 1
         print("Your Bill is ", p_price)
+        print(visual ** 10 * '-')
+
     else:
         print("Your Bill is ", p_price)
 
