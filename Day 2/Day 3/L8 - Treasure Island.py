334e753800f404d846273f48171fa28cb7dfb042
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2024-02-24 20:02:16 +0300

    Done Level 1 Added Level 3

--- Day 2/Day 3/L8 - Treasure Island.py
+++ Day 2/Day 3/L8 - Treasure Island.py
@@ -14,3 +14,22 @@ print(" Your mission is to find the treasure.")
 print((2**7)*'-')
 
 print("Make wise choices! Your life depends on it!")
+turn_1=str(input(print("You have arrived at a crossroad! Would you like to take a 'right' or 'left' turn? ")))
+if turn_1.lower() == 'left':
+    print("CONGRADULATIONS! On to the next level")
+    print("You have arrived at a the murky River Awach!")
+    swim_1=input(print("What do you want to do? Do you want to 'SWIM' or 'WAIT' "))
+    if swim_1.lower() == 'wait':
+        print("CONGRADULATIONS! ")
+        print('You have UNLOCKED LEVEL 3!')
+        print('You are face-to-face with THREE DOORS! Which door color do you want to turn into? ')
+    else:
+        print("Uh Oh! You are SWIMMING with the SHARKS!.")
+        print("Game Over")
+else:
+    print("Uh Oh! You have fallen into a bottomless PIT.")
+    print("GAME OVER!")    
+
+
+
+    
