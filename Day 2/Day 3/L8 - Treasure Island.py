49ea03a03a927faa6329ed919b1ff0b1def63d0b
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2024-02-24 19:27:31 +0300

    1. added header (007 ascii) 2. added intro

---
+++ Day 2/Day 3/L8 - Treasure Island.py
@@ -0,0 +1,16 @@
+#Treasure Island Code Buil
+print((2**7)*'-')
+print('''\n .d8888b.  .d8888b. 8888888888 
+d88P  Y88bd88P  Y88b      d88P 
+888    888888    888     d88P  
+888    888888    888    d88P   
+888    888888    888 88888888  
+888    888888    888  d88P     
+Y88b  d88PY88b  d88P d88P      
+ "Y8888P"  "Y8888P" d88P \n''')
+print((2**7)*'-')
+print("Welcome to Treasure Island.", end='')
+print(" Your mission is to find the treasure.") 
+print((2**7)*'-')
+
+print("Make wise choices! Your life depends on it!")
