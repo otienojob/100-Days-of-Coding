ab3b6fd3c440dae3e0cbc18a2d8e5ea77d384098
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2024-02-24 13:57:41 +0300

    V1-love calculator

--- Day 2/Day 3/L7 - Love Score.py
+++ Day 2/Day 3/L7 - Love Score.py
@@ -5,11 +5,48 @@ print(x**8 * '/')
 print("Welcome to Your Love Compatibility App")
 print(x**8 * '/')
 
-sp1_name=input(print("Enter your partner's name: "))
-sp2_name=input(print("Enter your name:"))
-name_array=list[sp1_name,sp2_name]
-print(name_array)
+names=[]
+names.insert(0,(input(print("Enter Your Partner's First Name"))))
+names.insert(1,(input(print("Enter Your Second Name"))))
+print(names)
 
 print(x**8 * '/')
+
+s_names=str(names)
+
+T=s_names.lower().count('t')
+R=s_names.lower().count('r')
+U=s_names.lower().count('u')
+E=s_names.lower().count('e')
+
+score1=T+R+U+E
+print(f'The letters making up your name appear {score1} times in the word "TRUE"')
+
+L=s_names.lower().count('l')
+O=s_names.lower().count('o')
+V=s_names.lower().count('v')
+E1=s_names.lower().count('e')
+
+score2=L+O+V+E1
+print(f'The letters making up your name appear {score2} times in the word "LOVE"')
+
+score3=str(score1) + str(score2)
+score4=int(score3)
+
+print(x**8 * '/')
+
+print("You have a total score of, ", score4, " Therefore: \n")
+
+if (score4<=10) or (score4>90):
+    print("The two of you go together like mentos")
+elif (score4>=40) and (score4<=50):
+    print("You are alright together")
+else:
+    print("Better Look for ALTERNATIVES")
+
+print(x**8 * '/')
+
 #count occurances of letters in 'true' and 'love'
+#t=names.count("s",)
+#print(t)
 
