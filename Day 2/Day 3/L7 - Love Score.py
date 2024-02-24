e0f466b67866189cfdff8cf7ee54a969dd616b36
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2024-02-24 11:53:42 +0300

    V1-love calculator

---
+++ Day 2/Day 3/L7 - Love Score.py
@@ -0,0 +1,15 @@
+#Love Calculator
+
+x=2
+print(x**8 * '/')
+print("Welcome to Your Love Compatibility App")
+print(x**8 * '/')
+
+sp1_name=input(print("Enter your partner's name: "))
+sp2_name=input(print("Enter your name:"))
+name_array=list[sp1_name,sp2_name]
+print(name_array)
+
+print(x**8 * '/')
+#count occurances of letters in 'true' and 'love'
+
