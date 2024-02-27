d1ed4472b3d958b6bd0dfe6054c48460e04769ce
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2024-02-27 16:05:19 +0300

    Mapping inputs to list

---
+++ Day 2/Day 4/L4 - Treasure Map Upgrade.py
@@ -0,0 +1,17 @@
+#Create an application that marks a spot on a map with an x
+
+print((2*6)*'**')
+#creating grids on the map
+line1=["A1","B1","C1"]
+line2=["A2","B2","C2"]
+line3=["A3","B3","C3"]
+
+map=[line1,line2,line3]
+hide_x=input()
+
+if hide_x=="A1":
+    hide_x.lower()=="a1"
+    map[0][0]="Y"
+
+print(map)
+
