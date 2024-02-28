1143d41412efdd8bddd2367d1f41283a768dcc21
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2024-02-28 09:48:24 +0300

    logic removed

--- Day 2/Day 4/L5 - Rock+Paper+Scissors.py
+++ Day 2/Day 4/L5 - Rock+Paper+Scissors.py
@@ -25,7 +25,16 @@ scissors = '''
 ---.__(___)
 '''
 
+import random
+
 game_images=[rock,paper,scissors]
 print((2**6)*'..')
 print("Welcome: Ready to Play the Rock, Paper, Scissors Game!")
 print(game_images)
+print((2**6)*'..')
+player_name=input(print("Start by Entering Your Name: "))
+gamer_input=int(input(print("Game On! Pick 0 for Rock, 1 for Paper and 2 for Scissors")))
+print("Invalid User Entry. Please try again")
+print(f"{player_name} , you have selected {game_images[gamer_input]} , now let's see the Computer's choice")
+computer_input=random.randrange(0,2)
+print(f"Computer's choice {game_images[computer_input]}")
