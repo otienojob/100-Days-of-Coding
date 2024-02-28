b8f49739cf28506187b9669b4fd001d92334aee8
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2024-02-28 08:45:18 +0300

    Added **

--- Day 2/Day 4/L4 - Treasure Map Upgrade.py
+++ Day 2/Day 4/L4 - Treasure Map Upgrade.py
@@ -9,6 +9,7 @@ line3=["A3","B3","C3"]
 map=[line1,line2,line3]
 hide_x=input()
 
+print((2*6)*'**')
 if hide_x=="A1":
     map[0][0]="X"
 elif hide_x=="B1":
@@ -27,6 +28,10 @@ elif hide_x=="B3":
     map[2][1]="X"
 elif hide_x == "C3":
     map[2][2]="X"
-    
+
+print((2*6)*'**')    
+
 print(f"{line1}\n{line2}\n{line3}")
+
+print((2*6)*'**')
 
