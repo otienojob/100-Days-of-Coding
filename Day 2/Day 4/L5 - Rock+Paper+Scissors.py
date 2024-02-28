6d67aab4c17990614343bee43491339e85892085
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2024-02-28 08:45:32 +0300

    part1: Image Display

---
+++ Day 2/Day 4/L5 - Rock+Paper+Scissors.py
@@ -0,0 +1,31 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game_images=[rock,paper,scissors]
+print((2**6)*'..')
+print("Welcome: Ready to Play the Rock, Paper, Scissors Game!")
+print(game_images)
