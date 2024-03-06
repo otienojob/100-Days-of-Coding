e90194fcf17cb088631cbe630b7f29d7864df621
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2024-03-06 12:19:37 +0300

    Added alphabet list

---
+++ Day 5/L7 Password Gen - v1.py
@@ -0,0 +1,3 @@
+#Help users generate a password
+upper_caps=['A','B,','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z']
+lower_caps=['a','b,','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z']
