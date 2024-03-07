047c7078574bff2d8d7ca83217e3b9613c6a9447
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2024-03-07 12:42:28 +0300

    x

--- Day 5/L7 Password Gen - v1.py
+++ Day 5/L7 Password Gen - v1.py
@@ -3,16 +3,15 @@ import random
 
 upper_caps=['A','B,','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z']
 lower_caps=['a','b,','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z']
-comb_case=[upper_caps,lower_caps]
+symbs=['~','!',]
 pass_holder=[]
 
 
 print((2**5)*'...' )
-print(comb_case)
 name_input=input(print('Enter Your Name: '))
 case_gen_req=int(input(print(f'How many letters would you like to generate {name_input}? ')))
 for x in range(0,case_gen_req):
-    pass_holder.append(random.choice(upper_caps))
+    pass_holder.append(random.choice(comb_case))
     print(pass_holder)
     
 #    pass_holder.append(random.randrange(upper_caps(0,9)))
