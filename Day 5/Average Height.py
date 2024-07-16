89c487395b65980201a27782ebc857af5da61375
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2024-07-16 13:51:23 +0300

    Update Average Height.py

--- Day 5/Average Height.py
+++ Day 5/Average Height.py
@@ -10,4 +10,11 @@ class_height=[]
 
 st_height_input=input(print("Enter the height of students in your class: "))
 class_height=[name.strip() for name in st_height_input.split()]
-print(class_height)
+print(class_height, " Values have been captured as String")
+#convert list (string) to integer
+
+new_class_height=[]
+for x in class_height:
+    new_class_height.append(int(x))
+print("")
+print("New Class List :", new_class_height)
