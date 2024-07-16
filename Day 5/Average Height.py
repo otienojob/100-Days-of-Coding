6d520dfadb673bd11b862121c676fed41eb4f85b
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2024-07-16 15:09:58 +0300

    Average

--- Day 5/Average Height.py
+++ Day 5/Average Height.py
@@ -10,7 +10,8 @@ class_height=[]
 
 st_height_input=input(print("Enter the height of students in your class: "))
 class_height=[name.strip() for name in st_height_input.split()]
-print(class_height, " Values have been captured as String")
+print(f'{class_height}, Note: values have been captured as string.', end=' ')
+print("Please convert values to integer!")
 #convert list (string) to integer
 
 new_class_height=[]
@@ -18,3 +19,16 @@ for x in class_height:
     new_class_height.append(int(x))
 print("")
 print("New Class List :", new_class_height)
+print((2**5)* '--')
+print("Now, to calculate the average height of a student in a class")
+sum_height=0
+n_students=0
+for n in new_class_height:
+    #get the sum of height
+    sum_height+=n
+    n_students+=1
+
+av_height=sum_height/n_students
+print(f' The Total Height Is: {sum_height}')
+print(f' The Number of Students Is: ', n_students)
+print(f' The Average Height Is: ', av_height)
