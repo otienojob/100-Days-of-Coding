cd99b0d6c172e5f435d70d1243868b74da4292bb
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2024-07-17 15:14:48 +0300

    Added exception handling

--- Day 5/#get sum of even numbers from 1 to x.py
+++ Day 5/#get sum of even numbers from 1 to x.py
@@ -3,3 +3,14 @@ print((2**5)* '--')
 print("Instructions:")
 print("'x' cannot be greater than '1000'")
 print((2**5)* '--')
+
+#error exception
+user_input=0
+while user_input<=0 and user_input>1000:
+    try:
+        user_input=int(input("Enter a value 'x'"))
+        if user_input<=0 or user_input>1000:
+            print("Invalid error. Enter a value between 1 and 1000")
+    except ValueError:
+        print("That's an invalid entry. Try again")
+#end of exception block
