bece9f662586f92a4b0a404e76981b0cd04043ef
Job Otian'ga <114091849+otienojob@users.noreply.github.com>
2024-07-17 09:52:08 +0300

    Update #Get the highest score from a list.py

--- Day 5/#Get the highest score from a list.py
+++ Day 5/#Get the highest score from a list.py
@@ -6,7 +6,17 @@ print("2: Press 'enter' to finish")
 print((2**5)* '--')
 
 class_scores=[]
+new_class_score=[]
 
 class_score_input=input(print("Enter the score of students in a class: "))
 class_scores=[name.strip() for name in class_score_input.split()]
-print(class_scores)
+#convert input of string to int
+new_class_score=list(map(int,class_scores))
+print(f'Student Scores: {new_class_score}')
+print((2**5)* '--')
+#getting the highest score from a list
+max_score=0
+for x in new_class_score:
+    if x > max_score:
+        max_score=x
+print(f'The highest score in the class is:, {max_score}')
